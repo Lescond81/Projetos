3194a948311117864c0027f86810614ecb41f3e4
Lescond81 <totortakashi@gmail.com>
2026-06-01 13:56:28 -0300

    criei um espaço para digitar e criar um pionto no grafico

--- PUC/MecSol/gridoir.js
+++ PUC/MecSol/gridoir.js
@@ -4,3 +4,16 @@ var f = board.create(
     [(x) => 0.5 * x ** 2 - 2 * x],
     {strokeWidth: 3}
 );
+
+// Parte para digitar no html e adicionar um ponto no gráfico
+const button = document.getElementById('Butao');
+const inputElement = document.getElementById('Funcao');
+
+// Botão para dar submit no valor digitado
+button.addEventListener('click', () => {
+  // Cria uma constante com o valor digitado
+  const inputValue = inputElement.value;
+  console.log(inputValue); 
+});
+
+let D = board.create('point',[inputValue, inputValue], {name: 'D'});
