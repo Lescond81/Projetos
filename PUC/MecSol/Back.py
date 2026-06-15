f25ed10feeb7d950690ea3ddc71380c73d634298
Lawrencio <vitulilaw@gmail.com>
2026-06-15 19:24:36 -0300

    Criando o back

---
+++ PUC/MecSol/Back.py
@@ -0,0 +1,78 @@
+import numpy as np
+import numpy.linalg as la
+import math 
+
+
+def Pontos():
+    A = []
+    B = []
+    C = []
+    D = []
+    E = []
+    F = []
+    G = []
+    H = []
+    I = []
+    J = []
+    K = []
+    L = []
+    M = []
+    N = []
+    O = []
+    P = []
+    Q = []
+    R = []
+    S = []
+    T = []
+    U = []
+    V = []
+    W = []
+    X = []
+    Y = []
+    Z = []
+ 
+    letras = (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z)
+
+Pontos()
+
+def Forcas_ext():
+    FA = []
+    FB = []
+    FC = []
+    FD = []
+    FE = []
+    FF = []
+    FG = []
+    FH = []
+    FI = []
+    FJ = []
+    FK = []
+    FL = []
+    FM = []
+    FN = []
+    FO = []
+    FP = []
+    FQ = []
+    FR = []
+    FS = []
+    FT = []
+    FU = []
+    FV = []
+    FW = []
+    FX = []
+    FY = []
+    FZ = []
+ 
+    F_ext = (FA, FB, FC, FD, FE, FF, FG, FH, FI, FJ, FK, FL, FM, FN, FO, FP, FQ, FR, FS, FT, FU, FV, FW, FX, FY, FZ)
+
+Forcas_ext()
+
+print(F_ext[0])
+
+for i in range(leng(F_ext)):
+    F_ext[i] = [A, 10, 30]
+    #if F_ext[i] != 0:
+        #temp = F_ext[i][2]*math.cos(F_ext[i][3])
+    break
+
+print(F_ext[0])
