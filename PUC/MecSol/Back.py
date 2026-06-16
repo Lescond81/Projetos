a58587d2976f9864b98adc611e1e8ff1078079a5
valenserra <valentinaserranoruiiz@gmail.com>
2026-06-15 21:56:15 -0300

    teste

--- PUC/MecSol/Back.py
+++ PUC/MecSol/Back.py
@@ -1,7 +1,38 @@
 import numpy as np
 import numpy.linalg as la
-import math 
+import math
 
+# funções
+
+def cosseno_diretor_x(i, j, nos):
+    dx = nos[j][0] - nos[i][0]
+    dy = nos[j][1] - nos[i][1]
+    L = math.hypot(dx, dy)
+    if L == 0:
+        return 0
+    return dx /L
+
+def cosseno_diretor_y(i, j, nos):
+    dx = nos[j][0] - nos[i][0]
+    dy = nos[j][1] - nos[i][1]
+    L = math.hypot(dx, dy)
+    if L == 0:
+        return 0
+    return dy / L
+
+# nós
+n_nos = int(input("Número de nós:"))
+nos    = []
+for i in range(n_nos):
+    x = float(input(f"x do nó {i}: "))
+    y = float(input(f"y do nó {i}: "))
+    nos.append([x, y])
+
+# barras
+n_barras = int(input("\nNúmero de barras: "))
+barras = []
+
+# 
 
 def Pontos():
     A = []
@@ -69,7 +100,7 @@ Forcas_ext()
 
 print(F_ext[0])
 
-for i in range(leng(F_ext)):
+for i in range(len(F_ext)):
     F_ext[i] = [A, 10, 30]
     #if F_ext[i] != 0:
         #temp = F_ext[i][2]*math.cos(F_ext[i][3])
