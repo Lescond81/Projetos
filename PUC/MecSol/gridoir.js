bc97fce97ea8f6a6891daf1c2cc88a75bda19113
Lescond81 <totortakashi@gmail.com>
2026-06-15 22:23:50 -0300

    Add functionality to connect points and apply forces

--- PUC/MecSol/gridoir.js
+++ PUC/MecSol/gridoir.js
@@ -1,28 +1,111 @@
-var A = board.create('point', [2, 1], {name: 'A'});
-var f = board.create(
-    'functiongraph',
-    [(x) => 0.5 * x ** 2 - 2 * x],
-    {strokeWidth: 3}
-);
-
 // Parte para digitar no html e adicionar um ponto no gráfico
 const button = document.getElementById('Butao');
 const inputElement = document.getElementById('Funcao');
+const Conectar = document.getElementById('connect');
+const Aplicar = document.getElementById('ap_forca');
+let nos = []; // Vetor com os valores dos nós
+let barras = []; // Vetor com a conexão entre os nós
+let forcas = []; // Vetor com as forças
 
 // Botão para dar submit no valor digitado
 button.addEventListener('click', () => {
-  const x =
-    Number(
-      document.getElementById("x").value
+    const x =
+      Number(
+        document.getElementById("x").value
       );
 
-  const y =
-    Number(
-      document.getElementById("y").value
+    const y =
+      Number(
+        document.getElementById("y").value
       );
-    board.create(
+
+    let pointo =
+      board.create(
         'point',
-        [x, y]
-    );
+        [x, y],
+        {
+            name:
+                nos.length
+        }
+      );
+    nos.push({
+        id: nos.length,
+        x,
+        y,
+        object: pointo
+      });
 });
 
+// Função para conectar 2 pontos entre si
+function conectar(id1, id2){
+
+    if(
+        !nos[id1] ||
+        !nos[id2]
+    ){
+        alert(
+            "Ponto inexistente"
+        );
+
+        return;
+    }
+
+    if(id1 === id2){
+
+        alert(
+            "Não conecte um ponto nele mesmo"
+        );
+
+        return;
+    }
+
+    const ponto1 =
+        nos[id1];
+
+    const ponto2 =
+        nos[id2];
+
+    const barra =
+        board.create(
+            "segment",
+            [
+                ponto1.object,
+                ponto2.object
+            ]
+        );
+
+    barras.push({
+        start: id1,
+        end: id2,
+        object: barra
+    });
+
+}
+
+Conectar.addEventListener('click', () => {
+      const prim =
+        Number(
+            document.getElementById("prim").value
+        );
+
+      const seg =
+        Number(
+            document.getElementById("seg").value
+        );
+
+    conectar(prim,seg);
+});
+
+Aplicar.addEventListener('click', () => {
+      let ponto =
+      Number(
+        document.getElementById("ponto").value
+      );
+
+      let modulo =
+      Number(
+        document.getElementById("modulo").value
+      );
+
+      
+});
